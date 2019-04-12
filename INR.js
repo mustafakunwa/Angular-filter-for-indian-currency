31170beba4f4596191d6f77af6b6d1710a5cf2d6
Mustafa kunwa <45129618+mustafakunwa@users.noreply.github.com>
2019-04-12 16:14:47 +0530

    Create INR.js

---
+++ INR.js
@@ -0,0 +1,21 @@
+myapp.filter('INR', function () {
+    return function (input) {
+        if (!isNaN(input)) {
+            var currencySymbol = '₹ ';
+            input = parseFloat(Math.round(input + 'e' + 2) + 'e-' + 2).toFixed(2);
+            var result = input.toString().split('.');
+
+            var lastThree = result[0].substring(result[0].length - 3);
+            var otherNumbers = result[0].substring(0, result[0].length - 3);
+            if (otherNumbers != '')
+                lastThree = ',' + lastThree;
+            var output = otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThree;
+
+            if (result.length > 1) {
+                output += "." + result[1];
+            }
+
+            return currencySymbol + output;
+        }
+    }
+});
